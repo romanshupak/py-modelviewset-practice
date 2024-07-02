1a0b288f664bc8ccc26afb8b73c3b16027e8d122
Roman Shupak <romanshupak808@gmail.com>
2024-07-02 13:33:32 +0300

    change AuthorSerializer to ModelSerializer

--- author/serializers.py
+++ author/serializers.py
@@ -2,17 +2,10 @@ from rest_framework import serializers
 from author.models import Author
 
 
-class AuthorSerializer(serializers.Serializer):
-    id_ = serializers.IntegerField(read_only=True)
-    first_name = serializers.CharField(max_length=64)
-    last_name = serializers.CharField(max_length=64)
-    pseudonym = serializers.CharField(
-        max_length=64,
-        allow_null=True,
-        allow_blank=True
-    )
-    age = serializers.IntegerField()
-    retired = serializers.BooleanField()
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = "__all__"
 
     def create(self, validated_data):
         return Author.objects.create(**validated_data)
